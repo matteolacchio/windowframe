9720db2f91ab6e6cfd10d12f2dc88adcd02f0b66
Davide397408 <davide.397408@gmail.com>
2022-12-30 23:09:13 +0100

    Minimal working version

--- development/index.js
+++ development/index.js
@@ -1,6 +1,17 @@
-import windowframeLib from "../lib/windowframe-lib";
+import { WindowFrameHost, WindowFrameChild } from "../lib/windowframe-lib";
 
-windowframeLib.TestFunction();
+const env_type = new URLSearchParams(window.location.search).get("type") ?? "";
 
+if (env_type == "child") {
+    console.log("Entrato child.");
+    const wfc = new WindowFrameChild("http://localhost:8888");
+    wfc.addEventListener("response", (event) => { console.log("gestito evento", event) });
+    wfc.postEvent("loaded");
 
-
+} else {
+    console.log("Entrato parent.");
+    const frame = document.getElementById("frame");
+    frame.src = "http://localhost:8888?type=child"
+    const wfh = new WindowFrameHost(frame, "http://localhost:8888");
+    wfh.addEventListener("loaded", () => { wfh.postEvent("response", typeof 1)});
+}
